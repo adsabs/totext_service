98858d0c743452e1d257368e31399258038f89f3
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2019-06-11 11:33:23 -0400

    Search with Flask-WTF

--- totext/routes.py
+++ totext/routes.py
@@ -1,13 +1,16 @@
 from flask import render_template, session
 from totext import app
+from totext.forms import QueryForm
 from datetime import datetime
 import requests
+import urllib
 
 PRODUCTION_URL = "https://prod.adsabs.harvard.edu/"
 DEVELOPMENT_URL = "https://dev.adsabs.harvard.edu/"
 ADS_URL = DEVELOPMENT_URL
 API_URL = ADS_URL+"v1/"
 BOOTSTRAP_SERVICE = API_URL+"accounts/bootstrap"
+SEARCH_SERVICE = API_URL+"search/query"
 API_TIMEOUT = 30
 
 def is_expired(auth):
@@ -31,13 +34,51 @@ def before_request():
         #   'expire_in': '2019-06-12T14:15:17.823482', 'refresh_token': 'itRUeo3vshekgyMYNMDxoGb84C6NTYoqjQ156xO9'}
         r = requests.get(BOOTSTRAP_SERVICE, cookies=session['cookies'], timeout=API_TIMEOUT)
         r.raise_for_status()
-        if r.ok:
-            r.cookies.clear_expired_cookies()
-            session['cookies'].update(r.cookies.get_dict())
-            session['auth'] = r.json()
+        r.cookies.clear_expired_cookies()
+        session['cookies'].update(r.cookies.get_dict())
+        session['auth'] = r.json()
 
-@app.route('/')
-@app.route('/index')
+def abstract(bibcode):
+    """
+    Retrieve abstract
+    """
+    headers = { "Authorization": "Bearer:{}".format(session['auth']['access_token']), }
+    params = urllib.urlencode({
+            'fl': 'title,bibcode,author,keyword,pub,aff,volume,year,[citations],property,pubdate,abstract,esources,data',
+            'q': 'citations(bibcode:{0})'.format(bibcode),
+            'rows': '25',
+            'sort': 'date desc',
+            'start': '0'
+            })
+    r = requests.get(SEARCH_SERVICE + "?" + params, headers=headers, cookies=session['cookies'], timeout=API_TIMEOUT)
+    r.raise_for_status()
+    r.cookies.clear_expired_cookies()
+    session['cookies'].update(r.cookies.get_dict())
+    return r.json()
+
+def search(q, sort="date desc, bibcode desc"):
+    """
+    Execute query
+    """
+    headers = { "Authorization": "Bearer:{}".format(session['auth']['access_token']), }
+    params = urllib.urlencode({
+                    'fl': 'title,abstract,comment,bibcode,author,keyword,id,citation_count,[citations],pub,aff,volume,pubdate,doi,pub_raw,page,links_data,property,esources,data,email,doctype',
+                    'q': '{0}'.format(q),
+                    'rows': '25',
+                    'sort': '{0}'.format(sort),
+                    'start': '0'
+                })
+    r = requests.get(SEARCH_SERVICE + "?" + params, headers=headers, cookies=session['cookies'], timeout=API_TIMEOUT)
+    r.raise_for_status()
+    r.cookies.clear_expired_cookies()
+    session['cookies'].update(r.cookies.get_dict())
+    return r.json()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Totext home', auth=session['auth'])
+    form = QueryForm()
+    if form.validate_on_submit():
+        results = search(form.query.data)
+        return render_template('index.html', title='Totext home', auth=session['auth'], form=QueryForm(), results=results['response'])
+    return render_template('index.html', title='Totext home', auth=session['auth'], form=QueryForm())
     #return "Hello, World!"
