d64e75b232865cd0b16d7e38731fd828a2add882
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2019-06-11 15:23:41 -0400

    Get method, sort, pagination and abstract links

--- totext/forms.py
+++ totext/forms.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField, SelectField
 from wtforms.validators import DataRequired
 
 class QueryForm(FlaskForm):
-    query = StringField('Query', validators=[DataRequired()])
+    q = StringField('q', validators=[DataRequired()])
+    #sort = StringField('sort', default="date desc, bibcode desc")
+    sort = SelectField('sort', default="date desc, bibcode desc",
+                       choices=[
+                           ('date desc, bibcode desc', 'Date (desc)'),
+                           ('citation_count desc, bibcode desc', 'Citations (desc)'),
+                           ('score desc, bibcode desc', 'Score (desc)'),
+                           ('date asc, bibcode asc', 'Date (asc)'),
+                           ('citation_count asc, bibcode asc', 'Citations (asc)'),
+                           ('score asc, bibcode asc', 'Score (asc)'),
+                       ])
+    rows = IntegerField('rows', default=5)
+    start = IntegerField('start', default=0)
     submit = SubmitField('Search')
